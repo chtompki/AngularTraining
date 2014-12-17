41fa4916e84327ca7d672b3b48b96422a0093945
Rob Tompkins <chtompki@gmail.com>
2014-12-17 15:07:01 -0500

    Finishing lab8

--- lab8/js/SuppliesController.js
+++ lab8/js/SuppliesController.js
@@ -1,10 +1,46 @@
-angular.module('LemonAide').controller('SuppliesController', ['$rootScope', '$scope', function($rootScope, $scope) {
+angular.module('LemonAide').controller('SuppliesController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
     $scope.page = {heading: "Supplies"};
 
-    $scope.supplies = {
-        lemonade: 80,
-        fruit: 24,
-        treats: 49
+    $http.get('http://localhost:8080/data/supplies.json').
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.success = true;
+            $scope.supplies = {
+                lemonade: data.initial.lemonadeQuantity,
+                lemonadeMax: 100,
+                fruit: data.initial.fruitQuantity,
+                fruitMax: 50,
+                treat: data.initial.treatQuantity,
+                treatMax: 50
+            };
+        }).
+        error(function(data, status, headers, config) {
+            $scope.success = false;
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+    $scope.resetSupplies = function() {
+        $http.get('http://localhost:8080/data/supplies.json').
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.success = true;
+                $scope.supplies = {
+                    lemonade: data.initial.lemonadeQuantity,
+                    lemonadeMax: 100,
+                    fruit: data.initial.fruitQuantity,
+                    fruitMax: 50,
+                    treat: data.initial.treatQuantity,
+                    treatMax: 50
+                };
+            }).
+            error(function(data, status, headers, config) {
+                $scope.success = false;
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
     }
 
 }]);
